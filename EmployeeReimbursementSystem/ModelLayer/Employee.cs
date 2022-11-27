4801ee1ecd323922d14d223082ba2317948dabbc
JasonTejadaRevature <jason263@revature.net>
2022-11-27 12:05:24 -0800

    Setup model layer for implementation.

---
+++ EmployeeReimbursementSystem/ModelLayer/Employee.cs
@@ -0,0 +1,17 @@
+/* JASON TEJADA    PROJECT 1 MODEL LAYER CLASS    REVATURE
+ * Desc:
+ *        This class defines the Employee object for the Employee Reimbursement
+ *        System. Datafields are: ID, role, email, and password. Service class
+ *        for procedures will be defined in the business layer.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelLayer;
+
+public class Employee {
+
+        
+}
