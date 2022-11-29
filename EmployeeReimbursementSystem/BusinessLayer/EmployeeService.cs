e1832af6c34a7654eb5e7a0fdcbe3b45d9411135
JasonTejadaRevature <jason263@revature.net>
2022-11-29 11:23:26 -0800

    First phase of RegisterEmployee started. Refactored some files. Did the appropriate referencing and setup the right methods to make everythign work in swagger. Might need to change the way our RegisterEmployee returns (instead of bool)

--- EmployeeReimbursementSystem/BusinessLayer/EmployeeService.cs
+++ EmployeeReimbursementSystem/BusinessLayer/EmployeeService.cs
@@ -1,3 +1,9 @@
+/* JASON TEJADA    PROJECT 1 BUSINESS LAYER CLASS    REVATURE 
+ * Desc:
+ *          This class defines the business logic for our employee
+ *          requirements for the ERS.
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,14 +11,35 @@ using System.Threading.Tasks;
 
 //Import our model
 using ModelLayer;
+// using RepositoryLayer;
+
 namespace BusinessLayer;
 
+// Interface to be used for dependency injection in the api layer
 public interface IEmployeeService {
     public bool RegisterEmployee(string email, string password);
 }
 
 public class EmployeeService : IEmployeeService {
     public bool RegisterEmployee(string email, string password) {
-        return false;
+        // TODO
+        // Need to get a list of employees from the repository layer
+        // Until then, use this list. When we use SQL, simply check
+        // and then do an insert query. No need to get list.
+        List<Employee> dbEmployee = new List<Employee>(); // TMP
+        int id = dbEmployee.Count() + 1; // TMP: query count of db 
+        bool result = true;
+        
+        // Validation
+        if(email.Length == 0 || password.Length == 0) result = false;
+        foreach(Employee entry in dbEmployee) {
+            if((entry.email).Equals(email))
+                result = false;
+        }
+
+        dbEmployee.Add(new Employee(id, email, password));
+        // PostToDatabase(dbEmployee)... add list, later this will be
+        // an insert query to insert an employee object
+        return result;
     }
 }
--- EmployeeReimbursementSystem/Tests.Business/XUnitEmployeeService.cs
+++ EmployeeReimbursementSystem/Tests.Business/XUnitEmployeeService.cs
@@ -1,22 +1,53 @@
 /* JASON TEJADA    PROJECT 1 TESTING LAYER FOR BUSINESS    REVATURE
-*/
+ * Desc:
+ *          This program tests our employee service class. Tests if the
+ *          employee service class does what we want. More specifically, test
+ *          if we can successfully implement our requirements for the business 
+ *          layer: RegisterEmployee
+ *          
+ */
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
+// Import necessary layers
+using ModelLayer;
+using BusinessLayer;
+// using RepositoryLayer;
+
 namespace Tests.Business;
 public class XUnitEmployeeService {
     [Theory]
     [InlineData("passtest@email.com", "123Pass")] // Pass
     [InlineData("failtest@email.com", "123Fail")] // Fail
-    [InlineData("failtest@email.com", "")] // Fail
+    [InlineData("failtest2@email.com", "")] // Fail
     [InlineData("", "123Fail")] // Fail
     [InlineData("", "")] // Fail
-    public void EmployeePostToDatabase(string email, string password) {
+
+ 
+    /* TODO: complete the test, first need to complete the database...
+     * Until then, we will fail the assertion on line 49 for test 2
+     */
+    public void RegisterValidEmployeeToDatabase(string email, string password) {
         // Arrange
+        List<Employee> dbEmployee = new List<Employee>();
+        dbEmployee.Add(new Employee(1, "failtest@email.com", "123"));
+        EmployeeService empServ = new EmployeeService();
+
         // Act
+        bool employeeRegistered = empServ.RegisterEmployee(email, password);
+
         // Assert
+        // Make sure user doesn't register with empty strings
+        if(email.Length == 0 || password.Length == 0) 
+            Assert.False(employeeRegistered);
+        
+        // Make sure the user is using a unique email
+        foreach(Employee e in dbEmployee) {
+            if((e.email).Equals(email)) 
+                Assert.False(employeeRegistered);
+        }
     }
 }
