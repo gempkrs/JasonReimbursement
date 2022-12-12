869a16cd93471bc73abde850150aee364345cb71
JasonTejadaRevature <jason263@revature.net>
2022-12-11 18:17:47 -0800

    Added loggers

---
+++ EmployeeReimbursementSystem/BusinessLayer/LoggerEmployeeValidation.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public interface ILoggerEmployeeValidation {
+        
+    }
+    
+    public class LoggerEmployeeValidation {
+        
+    }
+}
---
+++ EmployeeReimbursementSystem/RepositoryLayer/LoggerTicketRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer
+{
+    public interface ILoggerTicketRepository {
+        
+    }
+
+    public class LoggerTicketRepository {
+        
+    }
+}
